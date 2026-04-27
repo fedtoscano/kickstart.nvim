13a39b66c0007d6879b14c3bdbbbff0d1d768f42
Federico Toscano <federicotoscano611@gmail.com>
2026-04-27 09:49:08 +0200

    u

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,16 +13,16 @@ return {
         local set = vim.keymap.set
 
         -- Add or skip cursor above/below the main cursor.
-        set({"n", "x"}, "<up>", function() mc.lineAddCursor(-1) end)
-        set({"n", "x"}, "<down>", function() mc.lineAddCursor(1) end)
-        set({"n", "x"}, "<leader><up>", function() mc.lineSkipCursor(-1) end)
-        set({"n", "x"}, "<leader><down>", function() mc.lineSkipCursor(1) end)
+        set({"n", "x"}, "<up>", function() mc.lineAddCursor(-1) end, { desc = 'add cursor above'})
+        set({"n", "x"}, "<down>", function() mc.lineAddCursor(1) end, { desc = 'add cursor below'})
+        set({"n", "x"}, "<leader><up>", function() mc.lineSkipCursor(-1) end, { desc = 'skip cursor above'})
+        set({"n", "x"}, "<leader><down>", function() mc.lineSkipCursor(1) end, { desc = 'skip cursor below'})
 
         -- Add or skip adding a new cursor by matching word/selection
-        set({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end)
-        set({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1) end)
-        set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end)
-        set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
+        set({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end, { desc = 'add cursor to next match' })
+        set({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1) end, { desc = 'skip next cursor'})
+        set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end, { desc = 'add cursor to previous match'})
+        set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end, { desc = 'skip previous cursor'})
 
         -- Add and remove cursors with control + left click.
         set("n", "<c-leftmouse>", mc.handleMouse)
